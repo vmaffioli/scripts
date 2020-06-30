e6fa92e89e35434e5abec58f11663d9deb9a11e5
vmaffioli <vinimaffioli@gmail.com>
2020-06-30 02:59:10 -0300

    added file existence verification and turned script into a function to receive parameters

--- excel_csv_to_json/excel_csv_to_json.py
+++ excel_csv_to_json/excel_csv_to_json.py
@@ -8,21 +8,17 @@ def excel_csv_to_json(file_path):
         if re.search('\\b.xlsx\\b', file_path, re.IGNORECASE):
             df_excel = pd.read_excel(file_path)
             df_excel.to_json(file_path.replace(".xlsx", ".json"))
-            result = 0
-                    
+            result = 0                    
         elif re.search('\\b.xls\\b', file_path, re.IGNORECASE):
             df_excel = pd.read_excel(file_path)
             df_excel.to_json(file_path.replace(".xls", ".json"))
-            result = 0
-            
+            result = 0            
         elif re.search('\\b.csv\\b', file_path, re.IGNORECASE):
             df_csv = pd.read_csv(file_path)
             df_csv.to_json(file_path.replace(".csv", ".json"))
-            result = 0
-            
+            result = 0            
         else:
-            result = 1
-            
+            result = 1            
     else:
         result = 1     
             
